6dedf73a2655395010f332201f35fd690e64d6a1
xujh <xujhaosysu@163.com>
2015-11-03 17:25:42 +0800

    搜寻路劲成功

--- td/fortest/checkxml.py
+++ td/fortest/checkxml.py
@@ -1,8 +1,7 @@
 #-*- coding: UTF-8 -*-
-from __future__ import print_function
 from lxml import etree
 import  os
-xml_file_name = os.path.join(os.getcwd(), "test.xml")
+xml_file_name = os.path.join(os.getcwd(), "maServer.xml")
 doc = etree.parse(xml_file_name)
 root = doc.getroot()
 
@@ -10,16 +9,15 @@ qindegree = {}
 qoutdegree = {}
 mqset = set()
 mqlist = []
-sr_adj = {}
-global matrix
 matrix = []
 
 def checkServiceId():
+    print "checkServiceId"
     idset = set()
     for s in root.getiterator('services'):
         for sr in s.getiterator("service"):
             sid = s.getparent().tag + ":" + sr.get("id")
-            print ("id", sid)
+            print "id", sid
             if sid in idset:
                 print ("error!!! services name:", sr.get("name"), "id:", sid, "is duplicated")
                 return
@@ -27,6 +25,7 @@ def checkServiceId():
                 idset.add(sid)
 
 def checkQueueId():
+    print "checkQueueId"
     idset = set()
     for s in root.getiterator('queues'):
         for sr in s.getiterator("mqset"):
@@ -39,67 +38,37 @@ def checkQueueId():
                 idset.add(sid)
 
 def findAllMsgQueueDegree():
-    cnt = 0
     for s in root.getiterator('services'):
         for sr in s.getiterator("service"):
             for sf in sr.getiterator("svcfunc"):
-                inqueuelist=[]
-                outqueuelist=[]
-                if not (sf.get("inqueue") is None or sf.get("inqueue") == ""):
-                    inqueuelist = sf.get("inqueue").split(',')
-                    for iq in inqueuelist:
-                        iqstr = str(int(iq))
-                        iqindex = s.getparent().tag + ":" + iqstr
-                        mqset.add(iqindex)
+                if sf.get("inqueue") != "":
+                    for q in sf.get("inqueue").split(','):
+                        mqset.add(s.getparent().tag + ":" + str(int(q)))
+                if sf.get("outqueue") != "":
+                    for q in sf.get("outqueue").split(','):
+                        mqset.add(s.getparent().tag + ":" + str(int(q)))
 
-                        if iqindex in qoutdegree:
-                            qoutdegree[iqindex] += 1
-                        else:
-                            qoutdegree[iqindex] = 1
-
-                if not (sf.get("outqueue") is None or sf.get("outqueue") == ""):
-                    outqueuelist = sf.get("outqueue").split(',')
-                    for oq in outqueuelist:
-                        oqstr = str(int(oq))
-                        oqindex = s.getparent().tag + ":" + oqstr
-                        mqset.add(oqindex)
-                        if oqindex in qindegree:
-                            qindegree[oqindex] += 1
-                        else:
-                            qindegree[oqindex] = 1
                 if sf.get("inqueue") == "" or sf.get("outqueue") == "":
-                     cnt += 1
-                     mqset.add("*" + str(cnt))
+                    genReplaceNullStr(s, sr, sf)
+                    mqset.add(genReplaceNullStr(s, sr, sf))
 
-                sr_adj_value = sr.get("id") + "_" + sf.get("id") + "_" + sf.get("clsid")
-                if len(inqueuelist) > 0 and len(outqueuelist) > 0:
-                    for iq in inqueuelist:
-                        for oq in outqueuelist:
-                            iqstr = str(int(iq))
-                            iqindex = s.getparent().tag + ":" + iqstr
-                            oqstr = str(int(oq))
-                            oqindex = s.getparent().tag + ":" + oqstr
-                            sr_adj[iqindex + "," + oqindex] = sr_adj_value
+def genReplaceNullStr(s, sr, sf):
+    return s.getparent().tag + ":" + sr.get("id") + "_" + sf.get("id")
 
-                if len(inqueuelist) > 0 and len(outqueuelist) == 0:
-                    for iq in inqueuelist:
-                        iqstr = str(int(iq))
-                        iqindex = s.getparent().tag + ":" + iqstr
-                        sr_adj[iqindex + ","] = sr_adj_value
+def addIndegree(node):
+    if node in qindegree:
+        qindegree[node] += 1
+    else:
+        qindegree[node] = 1
 
-                if len(inqueuelist) == 0 and len(outqueuelist) > 0:
-                    for oq in outqueuelist:
-                        oqstr = str(int(oq))
-                        oqindex = s.getparent().tag + ":" + oqstr
-                        sr_adj["," + oqindex] = sr_adj_value
-
-                if not (sf.get("inqueue") is None or sf.get("inqueue") == "") or \
-                        not (sf.get("outqueue") is None or sf.get("outqueue") == ""):
-                    print ("parent", s.getparent().tag, "id", sr.get("id"), "inqueue:", sf.get("inqueue"), "outqueue:", sf.get("outqueue"))
+def addOutdegree(node):
+    if node in qoutdegree:
+        qoutdegree[node] += 1
+    else:
+        qoutdegree[node] = 1
 
 def getSrvAdj():
     mat = [["" for i in range(len(mqlist))] for j in range(len(mqlist))]
-    cnt = 0
     for s in root.getiterator('services'):
         for sr in s.getiterator("service"):
             for sf in sr.getiterator("svcfunc"):
@@ -113,61 +82,73 @@ def getSrvAdj():
                 if len(inqueuelist) > 0 and len(outqueuelist) > 0:
                     for iq in inqueuelist:
                         for oq in outqueuelist:
-                            iqstr = str(int(iq))
-                            iqindex = s.getparent().tag + ":" + iqstr
-                            oqstr = str(int(oq))
-                            oqindex = s.getparent().tag + ":" + oqstr
+                            iqindex = s.getparent().tag + ":" + str(int(iq))
+                            oqindex = s.getparent().tag + ":" + str(int(oq))
                             mat[mqlist.index(iqindex)][mqlist.index(oqindex)] = sr_adj_value
+                            addIndegree(oqindex)
+                            addOutdegree(iqindex)
 
                 if len(inqueuelist) > 0 and len(outqueuelist) == 0:
                     for iq in inqueuelist:
-                        cnt += 1
-                        iqstr = str(int(iq))
-                        iqindex = s.getparent().tag + ":" + iqstr
-                        mat[mqlist.index(iqindex)][mqlist.index("*" + str(cnt))] = sr_adj_value
+                        iqindex = s.getparent().tag + ":" + str(int(iq))
+                        mat[mqlist.index(iqindex)][mqlist.index(genReplaceNullStr(s, sr, sf))] = sr_adj_value
+                        addIndegree(genReplaceNullStr(s, sr, sf))
+                        addOutdegree(iqindex)
 
                 if len(inqueuelist) == 0 and len(outqueuelist) > 0:
                     for oq in outqueuelist:
-                        cnt += 1
-                        oqstr = str(int(oq))
-                        oqindex = s.getparent().tag + ":" + oqstr
-                        mat[mqlist.index("*" + str(cnt))][mqlist.index(oqindex)] = sr_adj_value
+                        oqindex = s.getparent().tag + ":" + str(int(oq))
+                        mat[mqlist.index(genReplaceNullStr(s, sr, sf))][mqlist.index(oqindex)] = sr_adj_value
+                        addIndegree(oqindex)
+                        addOutdegree(genReplaceNullStr(s, sr, sf))
     return mat
 
-def dfs(node, visit):
-    print (node,end = "")
-    visit[mqlist.index(node)] = True
+def isEnd(node, visit):
     for i in range(len(mqlist)):
         if matrix[mqlist.index(node)][i] != "" and not visit[i]:
-            print ("[" + matrix[mqlist.index(node)][i] + "]", "-> ",end = "")
-            dfs(mqlist[i],visit)
+            return False
+    return  True
 
 
-def traceQueue():
-    '''
-    入度为0或者在sr_adj中in为空的应为起点
-    :return:
-    '''
-    startnodes = mqset - set(qindegree)
-    for s in startnodes:
-        print( "begin<<", end = "")
-        visit = [False for i in range(len(mqlist))]
-        dfs(s, visit)
-        print (">>end")
+def traceRoute():
+    print "traceRoute"
+    startpoints = mqset - set(qindegree)
+    endpoints = mqset - set(qoutdegree)
 
+    print "startpoints:", startpoints
+    print "endpoints", endpoints
+    for s in startpoints:
+        for e in endpoints:
+            visit = [False for i in range(len(mqlist))]
+            path = []
+            schAllPath(s, e, visit, path)
 
+def schAllPath(v, t, visit, path):
+    if visit[mqlist.index(v)]:
+        return
+    path.append(v)
+
+    if v == t:
+        print "begin<<", path, ">>end"
+    else:
+        visit[mqlist.index(v)] = True
+        for i in range(len(mqlist)):
+            if matrix[mqlist.index(v)][i] != "" and not visit[i]:
+                path.append(matrix[mqlist.index(v)][i])
+                schAllPath(mqlist[i], t, visit, path)
+        visit[mqlist.index(v)] = False
 
 
 checkServiceId()
 checkQueueId()
-#traceQueue()
+#traceRoute()
 findAllMsgQueueDegree()
 mqlist = list(mqset)
 # print mqset
-print (mqlist)
+#print mqlist
 matrix = getSrvAdj()
-print (matrix)
-traceQueue()
+#print matrix
+traceRoute()
 
 
 
