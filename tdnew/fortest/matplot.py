9972aed8a13487e3146352a1d0e7d78bab8cd371
xujh <xujhaosysu@163.com>
2016-05-26 18:32:16 +0800

    e

--- tdnew/fortest/matplot.py
+++ tdnew/fortest/matplot.py
@@ -5,71 +5,70 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import datetime
+import matplotlib.dates as dt
+from matplotlib.lines import Line2D
+from matplotlib.dates import DateFormatter, WeekdayLocator,\
+    DayLocator, MONDAY
 
 
 class Twine(object):
-    def __init__(self):
-        # self._df = pd.DataFrame(columns={'high', 'low'})
+    def __init__(self, isUp):
+        self._isUp = isUp
+        self._df = pd.DataFrame(columns={'high', 'low'})
         # self._df.loc[pd.to_datetime(datetime.datetime.now())] = {'high':60.2, 'low':32.3}
         pass
 
+    def getDf(self):
+        return self._df
+
     #是否存在包含关系
-    def isContain(self, pre_line, curkline):
+    def isContain(self, prekline, curkline):
         #向左包含 n+1包含n
-        if curkline['high'] >= pre_line['high'] and curkline['low'] <= pre_line['low']:
-            return  True
+        if curkline['high'] >= prekline['high']  and curkline['low'] <= prekline['low']:
+            return True
         #向右包含 n包含n+1
-        elif curkline['high'] <= pre_line['high'] and curkline['low'] >= pre_line['low']:
-            return  True
+        elif curkline['high'] <= prekline['high'] and curkline['low'] >= prekline['low']:
+            return True
         else:
-            return  False
+            return False
 
     #判断是上升趋势还是下降趋势,True为上升趋势，False为下降趋势
-    def upOrDown(self, line1, line2):
+    def isUp(self, line1, line2):
         if line2['high'] >= line1['high'] and line2['low'] >= line1['low']:
             return True
         else:
             return False
 
+    def onNewKline(self, newkline):
+        self.procContain(newkline)
 
-    def contain(self, curkline):
-        if len(self._df) < 1:
-            self._df.loc[curkline['time']] = {'high':curkline['high'],
-                                              'low':curkline['low']}
-            return
+    def procContain(self, newkline):
+        if self._df.shape[0] < 1 or not self.isContain(self._df.ix[-1], newkline):
+            self._df.loc[newkline.name] = {'high': newkline['high'],
+                                           'low': newkline['low']}
+        elif self._df.shape[0] < 2:
+            self._procContain(self._isUp, newkline)
+        else:
+            self._procContain(self.isUp(self._df.ix[-2], self._df.ix[-1]), newkline)
 
-        if len(self._df) < 2:
-            if self.isContain(self._df.ix[-1], curkline):
-                pass
+    def _procContain(self, isUp, newkline):
+        #高点取高值，低点也取高值，简单的说就是“上升取高高”
+        if isUp:
+            self._df.loc[newkline.name] = {'high': max(self._df.ix[-1]['high'], newkline['high']),
+                                           'low': max(self._df.ix[-1]['low'], newkline['low'])}
+        #高点取低值，低点也取低值，简单的说就是“下降取低低”
+        else:
+            self._df.loc[newkline.name] = {'high': min(self._df.ix[-1]['high'], newkline['high']),
+                                           'low': min(self._df.ix[-1]['low'], newkline['low'])}
+        #处理完包含关系后，将前一个删除，只保留最终处理完的结果
+        self._df = self._df.drop(pd.Timestamp(self._df.index.values[-2]))
 
 
-        if self.isContain(self._df.ix[-1], curkline):
-            if self.upOrDown(self._df.ix[-2], self._df.ix[-1]):
-                self._df.loc[curkline['time']] = {'high': max(self._df.ix[-1]['high'], curkline['high']),\
-                                                  'low': max(self._df.ix[-1]['low'], curkline['low'])}
-            else:
-                self._df.loc[curkline['time']] = {'high': min(self._df.ix[-1]['high'], curkline['high']),\
-                                                  'low': min(self._df.ix[-1]['low'], curkline['low'])}
-            #处理完包含关系后，将前一个删除，只保留最终处理完的结果
-            self._df = self._df.drop(pd.Timestamp(self._df.index.values[-2]))
 
-    def procContain(self, df):
-        for i in xrange(df.shape[0]):
-            pass
-                                 
 
-def addLine(ax, df):
-    for i in xrange(df.shape[0]):
-        itDf = df.ix[i]
-        vline = Line2D(xdata=(i, i), ydata=(itDf['low'], itDf['high']))
-        ax.add_line(vline)
-        ax.autoscale_view()
-
-from matplotlib.lines import Line2D
-fig, ax = plt.subplots(3,1)
 
 hqdatadir = 'D:\TdxW_HuaTai\T0002\export'
-code = '000778'
+code = '000738'
 filepath = os.path.join(hqdatadir, (code + '.txt'))
 # if not os.path.exists(filepath):
 #     logger.critical("filepath %s does not exist", filepath)
@@ -84,15 +83,74 @@ df5mKline = pd.read_table(filepath,
                                  index_col='time',
                                  skiprows=2,
                                  skipfooter=1)
-dft = df5mKline[['low','high']]
-addLine(ax[0], dft)
 
-#plt.axhspan(xmin=0, xmax=1.2, facecolor='0.5', alpha=0.5)
-#plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
-plt.show()
+def addLine1(ax, df, **kwargs):
+    for i in xrange(df.shape[0]):
+        itDf = df.ix[i]
+        vline = Line2D(xdata=(i+0.5, i+0.5), ydata=(itDf['low'], itDf['high']), linewidth = 2, **kwargs)
+        ax.add_line(vline)
+        ax.autoscale_view()
+
+def picture1():
+    fig, ax = plt.subplots(3,1)
+    dft = df5mKline[['high','low']]
+    addLine1(ax[0], dft)
+
+    tw = Twine(True)
+    for i in xrange(dft.shape[0]):
+        tw.onNewKline(dft.ix[i])
 
 
-#import numpy as np
+    addLine1(ax[1], tw.getDf(), color = 'r')
+
+    addLine1(ax[2], dft, color = 'r')
+    addLine1(ax[2], tw.getDf(), color = 'b', linestyle = ':')
+
+
+
+    #plt.axhspan(xmin=0, xmax=1.2, facecolor='0.5', alpha=0.5)
+    #plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+    plt.show()
+
+def addLine2(ax, df, **kwargs):
+    for i in xrange(df.shape[0]):
+        itDf = df.ix[i]
+        vline = Line2D(xdata=(dt.date2num(itDf.name), dt.date2num(itDf.name)), ydata=(itDf['low'], itDf['high']), linewidth = 2, **kwargs)
+        ax.add_line(vline)
+        ax.autoscale_view()
+
+def picture2():
+    fig, ax = plt.subplots(2,1)
+    fig.subplots_adjust(bottom=0.2)
+    dft = df5mKline[['high','low']]
+    addLine2(ax[0], dft)
+
+    tw = Twine(True)
+    for i in xrange(dft.shape[0]):
+        tw.onNewKline(dft.ix[i])
+
+    addLine2(ax[1], tw.getDf(), color = 'r')
+
+
+    mondays = WeekdayLocator(MONDAY)        # major ticks on the mondays
+    alldays = DayLocator()              # minor ticks on the days
+    weekFormatter = DateFormatter('%b %d')  # e.g., Jan 12
+    dayFormatter = DateFormatter('%d')      # e.g., 12
+
+    for _ax in ax:
+        _ax.xaxis.set_major_locator(mondays)
+        _ax.xaxis.set_minor_locator(alldays)
+        _ax.xaxis.set_major_formatter(weekFormatter)
+        _ax.xaxis.set_minor_formatter(dayFormatter)
+        _ax.xaxis_date()
+        #ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
+        _ax.autoscale_view()
+
+    plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+    plt.show()
+
+picture2()
+
 #import matplotlib.pyplot as plt
 #
 #def f(t):
