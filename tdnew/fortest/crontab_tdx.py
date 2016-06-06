ec9a21662a9ff086306c1d4d5b45dd3c4ec56e31
xujhao <xujhaosysu@163.com>
2016-06-06 23:08:41 +0800

    e

--- tdnew/fortest/crontab_tdx.py
+++ tdnew/fortest/crontab_tdx.py
@@ -6,7 +6,7 @@ import time
 import pywinauto
 
 
-tdxExePath = "D:\TdxW_HuaTai\TdxW.exe"
+tdxExePath = "C:\TdxW_HuaTai\TdxW.exe"
 app = pywinauto.application.Application()
 app.start(tdxExePath)
 time.sleep(3)
@@ -16,6 +16,21 @@ hwnd_top = pywinauto.findwindows.find_window(title=u'华泰证券(通达信版)V
 hwnd_AfxWnd42 = pywinauto.findwindows.find_windows(top_level_only=False, class_name='AfxWnd42', parent=hwnd_top)
 market = app.window_(handle=hwnd_AfxWnd42[-3])
 market.ClickInput()
+
+td = pywinauto.findwindows.find_window(title = u'华泰证券(通达信版)V6.38 - [行情表-td]')
+td_AfxControlBar = pywinauto.findwindows.find_windows(top_level_only=False, class_name='AfxControlBar42', parent=td)
+app.window_(title = u'华泰证券(通达信版)V6.38 - [行情表-td]').SetFocus()
+tdHandle = app.window_(title = u'华泰证券(通达信版)V6.38 - [行情表-td]').window_(handle=td_AfxControlBar[0])
+tdRect = tdHandle.Rectangle()
+tdHandle.ClickInput(coords=(tdRect.left + 348, (tdRect.top + tdRect.bottom) / 2), absolute = True)
+
+time.sleep(2)
+ph = app.window_(title = u'盘后数据下载')
+phRect = ph.Rectangle()
+phtab = ph.window_(title='Tab2', class_name='SysTabControl32')
+phtab.ClickInput(coords=(phRect.left + 110, phRect.top + 42), absolute = True)
+
+
 
 #app.window_(title=u'华泰证券(通达信版)V6.38').window_()
 # app.connect(class_name='TdxW_MainFrame_Class')
