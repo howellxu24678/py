733b50ba1ee0614d9835bc9c4afd7c017b7d3787
xujh <xujhaosysu@163.com>
2016-06-29 10:34:10 +0800

    e

--- tdcron/main.py
+++ tdcron/main.py
@@ -21,43 +21,47 @@ logging.config.fileConfig(os.path.join(os.getcwd(), baseconfdir, loggingconf))
 logger = logging.getLogger("run")
 
 
+# def start():
+#     try:
+#         """主程序入口"""
+#         logger.info('start')
+#         global app
+#         app = QCoreApplication(sys.argv)
+#
+#         cf = ConfigParser.ConfigParser()
+#         cf.read(os.path.join(os.getcwd(), baseconfdir, businessconf))
+#
+#         global me
+#         me = Business(cf)
+#         me.start()
+#
+#         app.exec_()
+#     except BaseException,e:
+#         logger.exception(e)
+#
+# def stop():
+#     try:
+#         logger.info('stop')
+#         global me
+#         me.stop()
+#
+#         global app
+#         app.quit()
+#         app.deleteLater()
+#     except BaseException,e:
+#         logger.exception(e)
 def start():
-    try:
-        """主程序入口"""
-        logger.info('start')
-        global app
-        app = QCoreApplication(sys.argv)
-
-        cf = ConfigParser.ConfigParser()
-        cf.read(os.path.join(os.getcwd(), baseconfdir, businessconf))
-
-        global me
-        me = Business(cf)
-        me.start()
-
-        app.exec_()
-    except BaseException,e:
-        logger.exception(e)
-
+    print 'start'
 def stop():
-    try:
-        logger.info('stop')
-        global me
-        me.stop()
-
-        global app
-        app.quit()
-        app.deleteLater()
-    except BaseException,e:
-        logger.exception(e)
+    print 'stop'
 
 if __name__ == '__main__':
     from apscheduler.schedulers.blocking import BlockingScheduler
 
     sched = BlockingScheduler()
     # m = Main()
-    sched.add_job(start, 'cron', id='first', day_of_week ='1-5', hour = 9, minute = 10)
-    sched.add_job(stop, 'cron', id='second', day_of_week ='1-5', hour = 15, minute = 10)
+    sched.add_job(start, 'cron', id='first', day_of_week ='0-4', second = 10)
+    sched.add_job(stop, 'cron', id='second', day_of_week ='0-4', second = 20)
     sched.start()
 
 # app = None
