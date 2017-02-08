98408ffc960ae8820dfac3d69c3bb2a089a1d14a
xujhao <xujhaosysu@163.com>
2017-02-08 23:07:49 +0800

    还差验证码的识别

--- web_auto/91160/sele.py
+++ web_auto/91160/sele.py
@@ -40,14 +40,35 @@ def login(username, password):
         #logger.exception(e)
 
 #检查是否可以预约，传入医生的链接
-def is_bookable(ap):
+def is_bookable(book_locator):
     try:
         wait = WebDriverWait(driver, 2, 0.5)
-
-        book_locator = (By.XPATH, "//li[@class='liClassData'][%d]/div[@class='yuyuebor']/a" % ap)
         wait.until(EC.presence_of_element_located(book_locator))
-        driver.find_element(*book_locator)
-        #get_property("href")
+        return True
+    except BaseException,e:
+        print e
+        return False
+
+def book(book_href):
+    try:
+        wait = WebDriverWait(driver, 3, 0.5)
+        driver.get(book_href)
+        #默认选取可预约的最早的那个时间段（第一个）
+        time_locator = (By.XPATH, "//div[@class='fn-left yy-time']/ul[@class='fn-clear']/li[1]")
+        wait.until(EC.presence_of_element_located(time_locator))
+        driver.find_element(*time_locator).click()
+
+        #勾选医院预约规则
+        input_locator = (By.XPATH, "//div[@class='order-con']/div[@class='block p10']//input")
+        wait.until(EC.presence_of_element_located(input_locator))
+        driver.find_element(*input_locator).click()
+
+        #勾选现场支付 //div[@class='pay-list']/input[@name='pay_online' and @type='radio' and @value='0']/following-sibling::label
+        pay_locator = (By.XPATH, "//div[@class='pay-list']/input[@name='pay_online' and @type='radio' and @value='0']/following-sibling::label")
+        wait.until(EC.presence_of_element_located(pay_locator))
+        driver.find_element(*pay_locator).click()
+
+        #不勾选领取保险
 
         return True
     except BaseException,e:
@@ -57,20 +78,31 @@ def is_bookable(ap):
 
 # ap:1上午，2下午
 def autobook(link, ap):
-    driver.get(link)
-    while(True):
-        if not is_bookable(ap):
-            time.sleep(1)
-            driver.refresh()
-        else:
-            print "can book!"
-            break
-
-
-
+    try:
+        book_locator = (By.XPATH, "//li[@class='liClassData'][%d]/div[@class='yuyuebor']/a" % ap)
+        driver.get(link)
+        while(True):
+            if not is_bookable(book_locator):
+                driver.refresh()
+                time.sleep(1)
+            else:
+                print "can book!"
+                book_href = driver.find_element(*book_locator).get_property("href")
+                print "book_href %s" % book_href
+                #预约成功才退出循环
+                if book(book_href):
+                    break
+                else:
+                    #当进入了预定处理逻辑之后，网页的链接被改变了
+                    driver.get(link)
+    except BaseException,e:
+        print e
+    finally:
+        pass
+        #driver.close()
 
 login("18719286683", "0730xujhao")
-autobook("https://www.91160.com/doctors/index/docid-22611.html", 1)
+autobook("https://www.91160.com/doctors/index/docid-20428.html", 1)
 
 
 #driver.close()
